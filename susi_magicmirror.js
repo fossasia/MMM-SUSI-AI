bcae332345e7d243d2b6d7f378be34f7fb1328a2
betterclever <paliwal.pranjal83@gmail.com>
2017-07-01 20:55:43 +0530

    Added webpack to build and switched to new configuration

--- susi_magicmirror.js
+++ susi_magicmirror.js
@@ -1,5 +1,6 @@
 /* global Module */
 
+import * as SusiService from "./dist/renderer/index";
 /* Magic Mirror
  * Module: Susi Magic Mirror
  *
@@ -7,18 +8,47 @@
  * LGPL v2.1 Licensed.
  */
 
-Module.register("susi_magicmirror",{
+let susiMirror;
 
-	// Default module config.
-	defaults: {
-		text: "Ask me anything!!"
-	},
+Module.register("susi_magicmirror", {
 
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
-		return wrapper;
-	}
-	
+
+    start: function () {
+		this.sendSocketNotification("CONFIG", this.config);
+    },
+
+    getDom: function () {
+        const alexaWrapper = document.createElement("div");
+        alexaWrapper.setAttribute("id", "wrapper");
+
+        const susiVisualiserCanvas = document.createElement("canvas");
+        susiVisualiserCanvas.width = 400;
+        susiVisualiserCanvas.height = 300;
+        alexaWrapper.appendChild(susiVisualiserCanvas);
+
+        //TODO: Add Visualizations
+        susiMirror = new SusiService.SusiMirror(alexaWrapper, susiVisualiserCanvas, this.config, (event, payload) => {
+            this.sendSocketNotification(event, payload);
+        });
+
+        susiMirror.start();
+        return alexaWrapper;
+    },
+
+    getScripts: function () {
+        return [
+            this.file("dist/bundle.js"),
+        ];
+    },
+
+    getStyles: function () {
+        return [
+            this.file("styles/global.css"),
+        ];
+    },
+
+    socketNotificationReceived: function (notification, payload) {
+        Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+        susiMirror.receivedNotification(notification, payload);
+    },
 });
