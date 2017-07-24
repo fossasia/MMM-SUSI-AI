1f9b2081b7c2142f9656f63bf779e65b0e67254c
betterclever <paliwal.pranjal83@gmail.com>
2017-07-24 19:25:03 +0530

    Added React Dependencies

--- src/renderer/response-ui.tsx
+++ src/renderer/response-ui.tsx
@@ -2,10 +2,16 @@
  * Created by betterclever on 12/7/17.
  */
 import * as L from "leaflet";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {RSSComponent} from "./rss-component";
 
 export class ResponseUI {
 
-    constructor(private mainDiv: HTMLDivElement) {
+    public mainDiv: HTMLDivElement;
+
+    constructor(mainDiv: HTMLDivElement) {
+        this.mainDiv = mainDiv;
     }
 
     public changeText(text: string): void {
@@ -31,26 +37,45 @@ export class ResponseUI {
 
         const actions: Array<any> = susiResponse.answers[0].actions;
         for (const action of actions) {
+
             if (action.type === "answer") {
+
                 this.mainDiv.className = "thin bright";
                 this.mainDiv.setAttribute("style", "font-size: 2vw; margin: 40px");
                 const filteredText = this.removeLinks(action.expression);
                 const node = document.createTextNode(filteredText);
                 this.mainDiv.appendChild(node);
+
             } else if (action.type === "map") {
+
                 const mapDiv = document.createElement("div");
                 mapDiv.setAttribute("style", "width:300px; height: 300px; margin: 0 auto");
                 this.mainDiv.appendChild(mapDiv);
                 const latitude = parseFloat(action.latitude);
                 const longitude = parseFloat(action.longitude);
                 const map = L.map(mapDiv).setView([latitude, longitude], parseInt(action.zoom, 10));
+
                 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
                     attribution: "",
                     maxZoom: parseInt(action.zoom, 10)
                 }).addTo(map);
-                L.marker([51.5, -0.09]).addTo(map)
+
+                L.marker([latitude, longitude]).addTo(map)
                     .bindPopup("Here")
                     .openPopup();
+
+            } else if (action.type === "rss") {
+                const rssDiv = document.createElement("rss");
+                rssDiv.className = "rss-div";
+                this.mainDiv.appendChild(rssDiv);
+                ReactDOM.render(<RSSComponent/>, rssDiv);
+                // for (let i = 0; i < 5; i++) {
+                //     const div = document.createElement(`result:${i}div`);
+                //     div.className = "card";
+                //     rssDiv.appendChild(div);
+                // }
+                //
+                // this.mainDiv.appendChild(rssDiv);
             }
         }
     }
